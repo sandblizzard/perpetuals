fa10f07b500d522dc990fcc5903e997b6eb5f89f
kristohberg <kristoffer.hovland.berg@gmail.com>
2023-03-10 13:50:57 +0100

    feat: switch to BN rather than string

--- dapp/src/routes/swap/+page.svelte
+++ dapp/src/routes/swap/+page.svelte
@@ -3,7 +3,7 @@
 	import { WalletMultiButton } from '@svelte-on-solana/wallet-adapter-ui';
 	import autoAnimate from '@formkit/auto-animate';
 
-	import { BigNumber as BN } from 'bignumber.js';
+	import { BigNumber, type BigNumber as BN } from 'bignumber.js';
 	import TokenInput from '../../components/tokenInput.svelte';
 	import { page } from '$app/stores';
 	import { prettyAmount } from '../../helpers';
@@ -13,7 +13,6 @@
 
 	// Input
 	let leverage = 15;
-	let defaultTokenBalance = BN(0);
 
 	// tokens
 	let selectedBaseToken: Token | undefined;
@@ -23,28 +22,24 @@
 	let availableLiquidityUSD = '200000';
 
 	// Amounts
-	let baseTokenAmount: string | undefined;
-	let leveragedTokenAmount: string | undefined;
+	let baseTokenAmount: BN | undefined;
+	let leveragedTokenAmount: BN | undefined;
 
 	let showQuote = false;
 
-	function calculateLeveragedTokenAmount(prettyAmount: string | undefined, leverage: number) {
+	function calculateLeveragedTokenAmount(prettyAmount: BN | undefined, leverage: number) {
 		if (!prettyAmount) return undefined;
-		const amount = prettyAmount.replaceAll(',', '');
-		if (amount === '') {
-			return '0';
-		}
-		const amountBN = new BN(amount);
-		const amountUSD = amountBN.times(selectedBaseToken?.priceUSD ?? '0');
-		const leverageBN = new BN(leverage);
+
+		const amountUSD = prettyAmount.times(selectedBaseToken?.priceUSD ?? '0');
+		const leverageBN = new BigNumber(leverage);
 		const leverageUSD = amountUSD.times(leverageBN);
 		const leverageAmount = leverageUSD.dividedBy(selectedLeverageToken?.priceUSD ?? '0');
-		return leverageAmount.isNaN() ? '0' : leverageAmount.toString();
+		return leverageAmount;
 	}
 	// Update leverage token amount on baseTokenAmount change
 	$: leveragedTokenAmount = calculateLeveragedTokenAmount(baseTokenAmount, leverage);
 
-	$: showQuote = new BN(baseTokenAmount?.replaceAll(',', '')).gt(0) ? true : false;
+	$: showQuote = baseTokenAmount && baseTokenAmount.gt(0);
 </script>
 
 <div class="container flex flex-col gap-5">
@@ -77,8 +72,8 @@
 							<p class="text-base">From</p>
 							{#if $walletStore.connected && selectedBaseToken?.symbol}
 								<div class="flex flex-row items-center gap-1">
-									<p class="text-base">{defaultTokenBalance}</p>
-									<p class="text-sm">{`${selectedBaseToken?.symbol} balance`}</p>
+									<p class="text-base">{prettyAmount(selectedBaseToken.amount.toString())}</p>
+									<p class="text-sm">{`${selectedBaseToken?.symbol}`}</p>
 								</div>
 							{/if}
 						</div>
@@ -86,7 +81,6 @@
 							name={'base'}
 							bind:tokenAmount={baseTokenAmount}
 							bind:selectedToken={selectedBaseToken}
-							leverage={0}
 						/>
 					</div>
 				</div>
@@ -101,7 +95,6 @@
 							name="leverage"
 							bind:tokenAmount={leveragedTokenAmount}
 							bind:selectedToken={selectedLeverageToken}
-							bind:leverage
 						/>
 					</div>
 				</div>
@@ -126,11 +119,11 @@
 				<div class="flex flex-col gap-1">
 					<div class="flex flex-row justify-between">
 						<p class="text-base font-pixel">Entry price</p>
-						<p class="text-base font-pixel">{`$${prettyAmount(baseTokenAmount)}`}</p>
+						<p class="text-base font-pixel">{`$${prettyAmount(baseTokenAmount.toString())}`}</p>
 					</div>
 					<div class="flex flex-row justify-between">
 						<p class="text-base font-pixel">Exit price</p>
-						<p class="text-base font-pixel">{`$${prettyAmount(baseTokenAmount)}`}</p>
+						<p class="text-base font-pixel">{`$${prettyAmount(baseTokenAmount.toString())}`}</p>
 					</div>
 					<div class="flex flex-row justify-between">
 						<p class="text-base font-pixel">Borrow fee</p>
